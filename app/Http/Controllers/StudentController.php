cc0900ef9d68b2fee3c928e76a58458a342b42e9
Shruti Khandagale <sk123kh@gmail.com>
2023-10-10 01:48:47 +0530

    Student Controller Configuration

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,13 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        try
+        {
+        $student=Student::all();
+        return view('students.allstudents', compact('student'));
+        }
+        catch(Exception $e){
+            Echo "Error";
+        }
+    }
+
+    public function form(){
+        return view('students.stuform');
     }
 }

d4b692f1854a4253b92812442ae1ab8ae379abaa
Prerananale <naleprerana9@gmail.com>
2023-10-13 21:19:23 +0530

    bonafide format

---
+++ resources/views/bonafide/printbona.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="text-blue-900">
+        <div class="font-serif text-xl text-center font-extrabold">Sanjivani Academy</div>
+        <p class="font-sans text-sm text-center">(Non Grantable)</p>
+        <div class="text-center">
+          At:<b>Khirdi Ganesh</b>, Post : Yesgaon, Tal.Kopargaon, Dist.Ahmednagar (MS) 423601
+        </div>
+        <div class="flex w-[40%] mx-auto gap-28">
+          <p>Phone : 9075900900 </p>
+          <p class="mx-20">Fax : (02423)222682</p>
+        </div>
+        <div class="flex w-[40%] mx-auto gap-28">
+          <p>e-mail : sanjivanicbse@gmail.com</p>
+          <p>website : sanjivaniacademy.org.in</p>
+        </div>
+        <hr class="border-black w-[50%] mx-auto mt-2"/>
+
+          <div class="flex font-sans text-base p-4 w-[50%] mx-auto gap-[450px]">
+            <pre >SRES/SA/202  -202</pre>
+            <pre >Date: / /202</pre>
+          </div>
+          <div class="flex mb-10">
+          <button class="text-2xl rounded-full px-7 bg-blue-900 text-white font-bold mx-auto">BONAFIDE CERTIFICATE</button>
+          </div>
+          <div class="text-lg w-[50%] mx-auto">
+            <div> &emsp; &emsp; &ensp; This is to certify that Mast/Miss___________________is a</div> student of our school studying in Grade___Div.___in the academic year 202&emsp;-202&emsp; . His / Her date of birth as oer our school record is &emsp;/ &emsp;/
+            <div class="mt-4">(in words_______________________________). Place of birth___________Caste___________to the best of my knowledge, he/she bears a good moral character.</div>
+              </div>
+                <div class="w-[50%] mx-auto">
+                  <div class="mt-20">Place : <b>Kopargaon</b></div>
+                  <div class="flex gap-[550px]">
+                    <div>Date:</div>
+                    <div class="mx-auto">PRINCIPAL</div>
+                  </div>
+                </div>
+                </div>
+</body>
+
+</html>
